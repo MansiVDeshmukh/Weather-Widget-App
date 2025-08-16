97540e55ee99072128827004bad04c47bcb81306
Mansi Vinod Deshmukh <148883549+MansiVDeshmukh@users.noreply.github.com>
2025-08-16 23:27:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+//import './App.css'
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Weather from "./Weather.jsx"
+function App() {
+  return(
+    <>
+    <Weather></Weather>
+    </>
+  )
+}
+
+export default App
